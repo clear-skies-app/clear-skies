01bf63547443dedb662d956ec13b5c7743d09b0d
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-10 11:54:49 -0800

    observation page

--- src/ObservationPage/ObservationItem.js
+++ src/ObservationPage/ObservationItem.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import {Card, Button, Form, FormControl} from 'react-bootstrap'
 
 export default class ObservationItem extends Component {
+    state = {
+        observation: ''
+    }
+    handleFormSubmit = (e) => {
+        //post request goes here
+    }
+    handleObservationChange = (e) => {
+        this.setState({observation: e.target.value})
+    }
     render() {
         return (
-            <div>
-                Observation Item
-            </div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.image} />
+                <Card.Body>
+                    <Card.Title>{this.props.name}</Card.Title>
+                    <Form onSubmit={this.handleFormSubmit}>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Observation Notes</Form.Label>
+                            <FormControl as="textarea" aria-label="With textarea" onChange={this.handleObservationChange}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
         )
     }
 }
